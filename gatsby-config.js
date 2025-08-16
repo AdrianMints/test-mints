1e64b315352dc7118d8bea4709fcddf89823f3a2
Adrian Rosales <adrian@mints.cloud>
2025-08-15 18:33:45 -0600

    Changing to decap cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,7 @@
 module.exports = {
-  siteMetadata: {
-    title: `Mi sitio`,
-    siteUrl: `https://www.misitio.com`
-  },
+  siteMetadata: { title: `Mi sitio`, siteUrl: `https://www.misitio.com` },
   plugins: [
-    // otros plugins...
-    {
-      resolve: "gatsby-plugin-netlify-cms",
-      options: {
-        // modulePath es opcional, solo si tienes un archivo JS para inicializar widgets personalizados
-        // modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
+    // SIN gatsby-plugin-decap-cms
+    "gatsby-plugin-postcss",
   ],
 };
